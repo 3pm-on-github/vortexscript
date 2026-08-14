49bed8ceb0b0e60f07fbbfe1d223ed5b67c52150
3pm-on-github <3pm@threepm.xyz>
2026-08-14 20:51:48 +0200

    initial commit (mid-dev 😭)

---
+++ README.md
@@ -0,0 +1,34 @@
+# VortexScript v0.2
+VortexScript is a programming language that compiles to vortex maps. The file extension for VortexScript is .vs
+
+## TODO-LIST
+
+### Parts
+[x] Part variable type
+[x] Position (Vector3)
+[x] Rotation (Vector4)
+[x] Scale (Vector3)
+[x] Color (ColorRGBA)
+[x] Material (String)
+[ ] Group (Group Variable Type)
+[x] Cast Shadow (Boolean)
+[x] Anchored (Boolean)
+[x] Can Collide (Boolean)
+[x] Spawn Location (Boolean)
+[x] Baseplate (Boolean)
+[x] Truss (Boolean)
+[ ] Textures (Dictionary)
+
+"rotation": {
+    "x": 0.0,
+    "y": 0.0,
+    "z": 0.0,
+    "w": 1.0
+},
+"group": None,
+"textures": [
+    {
+        "face": "Top",
+        "kind": "Studs"
+    }
+]
---
+++ compiler.py
@@ -0,0 +1,145 @@
+import random, json, sys
+
+jsondata = {
+  "project_id": random.randbytes(16).hex(),
+  "parts": [],
+  "lights": [
+    {
+      "name": "Light",
+      "position": {
+        "x": 50.0,
+        "y": 80.0,
+        "z": 30.0
+      },
+      "rotation": {
+        "x": -0.39447272,
+        "y": 0.43916786,
+        "z": 0.22334667,
+        "w": 0.775654
+      },
+      "color": {
+        "r": 0.99999994,
+        "g": 0.99999994,
+        "b": 0.99999994,
+        "a": 1.0
+      },
+      "illuminance": 10000.0,
+      "shadows_enabled": True
+    }
+  ],
+  "groups": []
+}
+def getpartjson():
+    return {
+        "name": "Part",
+        "position": {
+            "x": 0.0,
+            "y": -0.5,
+            "z": 0.0
+        },
+        "rotation": {
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0,
+            "w": 1.0
+        },
+        "scale": {
+            "x": 1.0,
+            "y": 1.0,
+            "z": 1.0
+        },
+        "color": {
+            "r": 1.0,
+            "g": 1.0,
+            "b": 1.0,
+            "a": 1.0
+        },
+        "material": "Plastic",
+        "group": None,
+        "cast_shadow": True,
+        "anchored": False,
+        "can_collide": True,
+        "spawn_location": False,
+        "baseplate": False,
+        "truss": False,
+        "textures": [
+            {
+                "face": "Top",
+                "kind": "Studs"
+            }
+        ]
+    }
+
+def compiler(input, output):
+    script = open(input, "r").read()
+    variables = {}
+    i = 0
+    tocontinue = False
+    for line in script.split("\n"):
+        i+=1
+        line = line.strip()
+        for variable in variables:
+            if line.startswith(variable+"."):
+                name = line[len(variable+"."):].split("=")[0].strip()
+                value = line[len(variable)+1:].split("=")[1].strip()
+                if value == "true":
+                    variables[variable][name] = True
+                elif value == "false":
+                    variables[variable][name] = False
+                elif value.startswith("\"") or value.startswith("'"):
+                    variables[variable][name] = value[1:-1]
+                elif value.startswith("Vector3"):
+                    x = float(value[8:-1].split(",")[0].strip())
+                    y = float(value[8:-1].split(",")[1].strip())
+                    z = float(value[8:-1].split(",")[2].strip())
+                    variables[variable][name]["x"] = x
+                    variables[variable][name]["y"] = y
+                    variables[variable][name]["z"] = z
+                elif value.startswith("Vector4"):
+                    x = float(value[8:-1].split(",")[0].strip())
+                    y = float(value[8:-1].split(",")[1].strip())
+                    z = float(value[8:-1].split(",")[2].strip())
+                    w = float(value[8:-1].split(",")[3].strip())
+                    variables[variable][name]["x"] = x
+                    variables[variable][name]["y"] = y
+                    variables[variable][name]["z"] = z
+                    variables[variable][name]["w"] = w
+                elif value.startswith("ColorRGBA"):
+                    r = float(int(value[10:-1].split(",")[0].strip()) / 255)
+                    g = float(int(value[10:-1].split(",")[1].strip()) / 255)
+                    b = float(int(value[10:-1].split(",")[2].strip()) / 255)
+                    a = float(int(value[10:-1].split(",")[3].strip()) / 255)
+                    variables[variable][name]["r"] = r
+                    variables[variable][name]["g"] = g
+                    variables[variable][name]["b"] = b
+                    variables[variable][name]["a"] = a
+                else:
+                    print(f"Error: unknown value at line {str(i)} \"{value}\"")
+                    exit()
+                tocontinue = True
+        if tocontinue:
+            tocontinue = False
+            continue
+        if line.startswith("part"):
+            name = line[4:].strip().split("=")[0].strip()
+            value = line[4:].strip().split("=")[1].strip()
+            if value == "Part()":
+                variables[name] = getpartjson()
+                variables[name]["type"] = "Part"
+                variables[name]["name"] = name
+        elif line.startswith("//") or line == "":
+            pass
+        else:
+            print(f"Error: unknown line at line {str(i)} \"{line}\"")
+            exit()
+    for variable in variables:
+        if variables[variable]["type"] == "Part":
+            variables[variable].pop("type")
+            jsondata["parts"].append(variables[variable])
+    open(f"{output}", "w").write(json.dumps(jsondata))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Error: not enough arguments!\nUsage: py compiler.py [input filename] [output filename]")
+        exit()
+    compiler(sys.argv[1], sys.argv[2])
